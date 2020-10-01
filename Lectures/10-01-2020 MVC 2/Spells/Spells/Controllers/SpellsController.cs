23a1afbbd1558efa2077f8f1ac8e3b91e8eafa95
Steven Yackel <spazard1@gmail.com>
2020-10-01 13:57:18 -0500

    Adds base MVC 2 lecture

---
+++ Lectures/10-01-2020 MVC 2/Spells/Spells/Controllers/SpellsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Spells.Controllers
+{
+    
+    public class SpellsController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult NewRoute()
+        {
+            return new ContentResult()
+            {
+                StatusCode = (int) HttpStatusCode.Accepted,
+                Content = "You accessed the new route!"
+            };
+        }
+    }
+}
