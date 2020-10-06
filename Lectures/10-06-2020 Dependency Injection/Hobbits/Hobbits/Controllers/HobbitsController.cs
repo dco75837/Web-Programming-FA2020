424d267f8a30d4436f2685cef87bba8b85d7b45e
Steven Yackel <styackel@microsoft.com>
2020-10-06 11:50:36 -0500

    Adds base Dependency Injection

---
+++ Lectures/10-06-2020 Dependency Injection/Hobbits/Hobbits/Controllers/HobbitsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hobbits.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Hobbits.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HobbitsController : ControllerBase
+    {
+
+
+        public HobbitsController()
+        {
+
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "hobbit";
+        }
+    }
+}
